5bf73296ec72ac0c05e5cf471d19216aa4f9848b
“JHuang19960705 <abc850705@gmail.com>
2023-11-18 12:40:30 +0800

    改成jquery

--- RPS/js/RPS.js
+++ RPS/js/RPS.js
@@ -4,8 +4,80 @@ const score = JSON.parse(localStorage.getItem('score')) || {
   ties: 0
 }
 
+// 手動
+$(".js-move-button").click(playerMove => {
+    let move = playerMove.currentTarget.dataset.playMove;
+    playGame(move);
+})
+function playGame(playerMove) {
+    const computerMove = pickComputerMove();
+    let result = '';
+    if (playerMove === 'Scissors') {
+        if (computerMove === 'Rock') {
+            result = 'You lose.';
+        } else if (computerMove === 'Paper') {
+            result = 'You win.';
+        } else if (computerMove === 'Scissors') {
+            result = 'Tie.';
+        }
+  
+    } else if (playerMove === 'Paper') {
+        if (computerMove === 'Rock') {
+            result = 'You win.';
+        } else if (computerMove === 'Paper') {
+            result = 'Tie.';
+        } else if (computerMove === 'Scissors') {
+            result = 'You lose.';
+        }
+  
+    } else if (playerMove === 'Rock') {
+        if (computerMove === 'Rock') {
+            result = 'Tie.';
+        } else if (computerMove === 'Paper') {
+            result = 'You lose.';
+        } else if (computerMove === 'Scissors') {
+            result = 'You win.';
+        }
+    }
+  
+    if (result === 'You win.') {
+        score.wins += 1;
+    } else if (result === 'You lose.') {
+        score.losses += 1;
+    } else if (result === 'Tie.') {
+        score.ties += 1;
+    }
+  
+  
+    localStorage.setItem('score', JSON.stringify(score));
+    
+    updateScoreElement();
+  
+    document.querySelector('.js-result')
+    .innerHTML = result;
+  
+    document.querySelector('.js-move')
+    .innerHTML = `You
+    <img src="img/${playerMove}-emoji.png" alt=""
+    class="move-icon">
+    <img src="img/${computerMove}-emoji.png" alt=""
+    class="move-icon">
+    Computer
+    `;
+  
+}
+
+//更新分數 
 updateScoreElement();
 
+function updateScoreElement(){
+    $('.js-score').html(
+        `Wins: ${score.wins}, Lossses: ${score.losses}, Ties: ${score.ties}`
+    );
+  }
+
+
+// 電腦自動互玩
 let isAutoplaying = false;
 let intervalId; // 放入clearInterval()裡面
 
@@ -23,73 +95,7 @@ function autoPlay() {
 };
 
 
-function playGame(playerMove) {
-  const computerMove = pickComputerMove();
-  let result = '';
-  if (playerMove === 'Scissors') {
-      if (computerMove === 'Rock') {
-          result = 'You lose.';
-      } else if (computerMove === 'Paper') {
-          result = 'You win.';
-      } else if (computerMove === 'Scissors') {
-          result = 'Tie.';
-      }
-
-  } else if (playerMove === 'Paper') {
-      if (computerMove === 'Rock') {
-          result = 'You win.';
-      } else if (computerMove === 'Paper') {
-          result = 'Tie.';
-      } else if (computerMove === 'Scissors') {
-          result = 'You lose.';
-      }
-
-  } else if (playerMove === 'Rock') {
-      if (computerMove === 'Rock') {
-          result = 'Tie.';
-      } else if (computerMove === 'Paper') {
-          result = 'You lose.';
-      } else if (computerMove === 'Scissors') {
-          result = 'You win.';
-      }
-  }
-
-  if (result === 'You win.') {
-      score.wins += 1;
-  } else if (result === 'You lose.') {
-      score.losses += 1;
-  } else if (result === 'Tie.') {
-      score.ties += 1;
-  }
-
-
-  localStorage.setItem('score', JSON.stringify(score));
-  
-  updateScoreElement();
-
-  document.querySelector('.js-result')
-  .innerHTML = result;
-
-  document.querySelector('.js-move')
-  .innerHTML = `You
-  <img src="img/${playerMove}-emoji.png" alt=""
-  class="move-icon">
-  <img src="img/${computerMove}-emoji.png" alt=""
-  class="move-icon">
-  Computer
-  `;
-
-}
-
-
-function updateScoreElement(){
-  document.querySelector('.js-score')
-      .innerHTML = 
-      `Wins: ${score.wins}, Lossses: ${score.losses}, Ties: ${score.ties}`;
-}
-
-
-
+// 電腦出的拳
 function pickComputerMove() {
   const randomNumber = Math.random();
   let computerMove = '';
