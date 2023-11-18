0b1d32432f3fe4dbf5be20b25589b1e543564a3b
“JHuang19960705 <abc850705@gmail.com>
2023-11-18 13:50:38 +0800

    骰子比賽呈現結果

--- Dicee/dicee.js
+++ Dicee/dicee.js
@@ -3,18 +3,32 @@
 
 // console.log(Math.floor(Math.random()*10)%6 + 1);
 
-
+let dicee1Score = 1;
+let dicee2Score = 1;
 
 
 $(".js-dicee-1").on("click", (diceeHTML) =>{
-  let randomNumber1 = Math.floor(Math.random()*10)%6 + 1;
-  let number = `${randomNumber1}`;
+  dicee1Score = Math.floor(Math.random()*10)%6 + 1;
+  let number = `${dicee1Score}`;
   diceeHTML.target.src = `../Dicee/images/dice${number}.png`
+  result();
 });
 
 
 $(".js-dicee-2").on("click", (diceeHTML) =>{
-  let randomNumber1 = Math.floor(Math.random()*10)%6 + 1;
-  let number = `${randomNumber1}`;
+  dicee2Score = Math.floor(Math.random()*10)%6 + 1;
+  let number = `${dicee2Score}`;
   diceeHTML.target.src = `../Dicee/images/dice${number}.png`
+  result();
 });
+
+
+function result() {
+  if(dicee1Score > dicee2Score){
+    $("h1").html("Player1 WIN!!")
+  }else if(dicee1Score < dicee2Score){
+    $("h1").html("Player2 WIN!!")
+  }else if(dicee1Score == dicee2Score){
+    $("h1").html("Draw!")
+  }
+}
